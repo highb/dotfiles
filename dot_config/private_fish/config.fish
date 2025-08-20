398541e8890b2bf8d13f8d61e8fd728098600c1c
Brandon High <759848+highb@users.noreply.github.com>
2025-08-20 15:53:25 -0700

    fixing EDITOR, adding chezmoi wrapper for laziness

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,4 +6,16 @@ function fish_greeting
     echo "🐟 Something fishy this way comes"
 end
 
+function chezmoi_edit
+    chezmoi edit --apply $argv
+end
+
+function chezmoi_commit_push
+    chezmoi git add .
+    chezmoi git commit
+    chezmoi git push
+end
+
 mise activate fish | source
+
+set -gx EDITOR nvim
